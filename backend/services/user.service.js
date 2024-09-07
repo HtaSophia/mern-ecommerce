1a5dc8a7134f4729ca68e6926d08dd79b7200e82
Aghata Sophia <aghatasophia@gmail.com>
2024-09-06 22:35:35 -0400

    refactor(auth): Create a UserService to separate the main logic part from the Auth controller

---
+++ backend/services/user.service.js
@@ -0,0 +1,55 @@
+import User from "../models/user.model.js";
+
+/**
+ * @typedef PartialUser
+ * @property {string} _id
+ * @property {string} name
+ * @property {string} email
+ * @property {string} password
+ * @property {string} role
+ **/
+
+export default class UserService {
+    /**
+     * Creates a new user in the database and returns a user object with a subset of the user's data.
+     * @param {{name: string, email: string, password: string}} user - A new user object.
+     * @return {Promise<PartialUser>} A user object with a subset of the user's data: {_id, name, email, cartItems, role}.
+     */
+    static async create(user) {
+        const { name, email, password } = user;
+
+        const newUser = await User.create({ name, email, password });
+
+        return {
+            _id: newUser._id,
+            name: newUser.name,
+            email: newUser.email,
+            cartItems: newUser.cartItems,
+            role: newUser.role,
+        };
+    }
+
+    /**
+     * Finds a user by email and returns the user object if found.
+     * @param {string} email - The email of the user to find.
+     * @return {Promise<User | null>} The user object if found, or null if the user does not exist.
+     */
+    static async findByEmail(email) {
+        return User.findOne({ email });
+    }
+
+    /**
+     * Logs in an existing user and returns a user object with a subset of the user's data.
+     * @param {{email: string, password: string}} user - The email and password of the user to log in.
+     * @return {Promise<Object>} A user object with a subset of the user's data: {_id, name, email, cartItems, role}.
+     * @throws {Error} If the email or password is invalid.
+     */
+    static async login({ email, password }) {
+        const user = await User.findOne({ email });
+        const passwordMatches = user && (await user.matchPassword(password));
+
+        if (!user || !passwordMatches) {
+            throw new Error("Invalid email or password");
+        }
+    }
+}
