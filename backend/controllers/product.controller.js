ddb5375ec8d45f823ce2ea6bae46cbe99f8c5e3b
Aghata Sophia <aghatasophia@gmail.com>
2024-09-09 15:32:35 -0400

    feat(product): Add getFeaturedProducts method to get from redis/mongodb all the featured products

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,11 +1,34 @@
 import ProductService from "../services/product.service.js";
 
-export const getAllProducts = async (req, res) => {
+/**
+ * Retrieves all products in the database.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ */
+export const getAllProducts = async (_req, res) => {
     try {
         const products = await ProductService.findAll();
         res.status(200).json({ data: { products }, message: "Products retrieved successfully" });
     } catch (error) {
-        console.error("Error getting products (getAllProducts):", error);
+        console.error("Error (getAllProducts):", error);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+/**
+ * Retrieves all featured products in the database, stored in Redis for faster access.
+ * Featured products are products with the "isFeatured" property set to true.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ */
+export const getFeaturedProducts = async (_req, res) => {
+    try {
+        const products = await ProductService.findAllFeatured();
+        res.status(200).json({ data: { products }, message: "Featured products retrieved successfully" });
+    } catch (error) {
+        console.error("Error (getFeaturedProducts):", error);
         res.status(500).json({ message: error.message });
     }
 };
--- backend/services/product.service.js
+++ backend/services/product.service.js
@@ -1,3 +1,4 @@
+import redis from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
 export default class ProductService {
@@ -5,7 +6,28 @@ export default class ProductService {
      * Retrieves all products in the database.
      * @return {Promise<Product[]>} An array of Product objects.
      */
-    static async findAll() {
-        return Product.find({});
+    static async findAll(filter = {}) {
+        return Product.find().lean();
+    }
+
+    /**
+     * Retrieves all featured products in the database.
+     * If the featured products are not cached in Redis, they are retrieved from the database
+     * and stored in Redis with a TTL of 3 hours.
+     * @return {Promise<Product[]>} An array of Product objects.
+     */
+    static async findAllFeatured() {
+        const cachedFeaturedProducts = await redis.get("featured_products");
+
+        if (cachedFeaturedProducts) {
+            return JSON.parse(cachedFeaturedProducts);
+        }
+
+        const featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+        // Store the featured products in Redis with a TTL of 3 hours
+        await redis.set("featured_products", JSON.stringify(featuredProducts), "EX", 60 * 60 * 3);
+
+        return featuredProducts;
     }
 }
