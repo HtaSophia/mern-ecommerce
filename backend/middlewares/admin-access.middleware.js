c1dfcda461b1e6a3453ef36693cba758b1f70699
Aghata Sophia <aghatasophia@gmail.com>
2024-09-09 14:13:09 -0400

    feat(middleware): Create admin access middleware to check if the user is an admin

---
+++ backend/middlewares/admin-access.middleware.js
@@ -0,0 +1,7 @@
+export const adminAccessMiddleware = async (req, res, next) => {
+    if (!req.user?.role === "admin") {
+        return res.status(403).json({ message: "Unauthorized: Access denied" });
+    }
+
+    next();
+};
