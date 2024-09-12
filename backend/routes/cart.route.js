655dfff8ab597c1c41b5fe734e630a5c9a738e35
Aghata Sophia <aghatasophia@gmail.com>
2024-09-12 15:02:25 -0400

    feat(coupon): Create coupon routes

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,13 +1,12 @@
 import express from "express";
 
-import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { getCart, addItemToCart, updateItemQuantity, removeItemFromCart } from "../controllers/cart.controller.js";
 
 const router = express.Router();
 
-router.get("/", authMiddleware, getCart);
-router.post("/item", authMiddleware, addItemToCart);
-router.put("/item/:itemId", authMiddleware, updateItemQuantity);
-router.delete("/item/:itemId", authMiddleware, removeItemFromCart);
+router.get("/", getCart);
+router.post("/item", addItemToCart);
+router.put("/item/:itemId", updateItemQuantity);
+router.delete("/item/:itemId", removeItemFromCart);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 
+import { authMiddleware } from "./middlewares/auth.middleware.js";
 import authRoutes from "./routes/auth.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
@@ -18,7 +20,8 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api/cart", cartRoutes);
+app.use("/api/cart", authMiddleware, cartRoutes);
+app.use("/api/coupons", authMiddleware, couponRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`);
