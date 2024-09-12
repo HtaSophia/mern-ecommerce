f39976f08516e1629ce9410df9ef0795f129eb7f
Aghata Sophia <aghatasophia@gmail.com>
2024-09-12 15:01:07 -0400

    feat(coupon): Create method to get validate the coupon

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -9,3 +9,16 @@ export const getCoupon = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+export const validateCoupon = async (req, res) => {
+    const { couponCode } = req.body;
+
+    try {
+        const coupon = await CouponService.validateCoupon(couponCode);
+        res.status(200).json({ data: { coupon }, message: "Coupon is valid" });
+    } catch (error) {
+        // TODO Add error handling
+        console.error("Error (validateCoupon):", error);
+        res.status(500).json({ message: error.message });
+    }
+};
--- backend/services/coupon.service.js
+++ backend/services/coupon.service.js
@@ -9,4 +9,27 @@ export default class CouponService {
     static async getCoupon(userId) {
         return Coupon.findOne({ ownerId: userId, isActive: true }).lean();
     }
+
+    /**
+     * Validates a coupon code by checking if the coupon exists and is still active.
+     * @param {string} code - The coupon code to validate.
+     * @return {Promise<Object>} A validated coupon object with a subset of the coupon's data: {code, discountPercentage}.
+     * @throws {Error} If the coupon is not found or has expired.
+     */
+    static async validateCoupon(code) {
+        const coupon = await Coupon.findOne({ code, isActive: true });
+
+        if (!coupon) {
+            throw new Error("Coupon not found");
+        }
+
+        if (coupon.expirationDate < new Date()) {
+            coupon.isActive = false;
+            await coupon.save();
+
+            throw new Error("Coupon expired");
+        }
+
+        return { code: coupon.code, discountPercentage: coupon.discountPercentage };
+    }
 }
