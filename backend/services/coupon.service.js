a168248455809bea7ced402cdf4abec103bc5668
Aghata Sophia <aghatasophia@gmail.com>
2024-09-13 21:38:45 -0400

    feat(coupon): Create method to create a coupon to a specific user

--- backend/services/coupon.service.js
+++ backend/services/coupon.service.js
@@ -1,13 +1,35 @@
+import stripe from "../lib/stripe";
+
 import Coupon from "../models/coupon.model.js";
+import { compareDates } from "../utils/date.util.js";
+import { generateRandomString } from "../utils/random.util.js";
 
 export default class CouponService {
+    /**
+     * Creates a coupon and adds it to the given user's coupons.
+     * Also creates a coupon in Stripe.
+     * @param {{ discountPercentage: number, expirationDate: Date, userId: string }} couponInfo - The info of the coupon to create.
+     * @return {Promise<Coupon>} The created coupon.
+     */
+    static async createToUser({ discountPercentage, expirationDate, userId }) {
+        const code = `GIFT${generateRandomString().toUpperCase()}`;
+
+        const stripeCoupon = await stripe.coupons.create({
+            duration: "once",
+            percent_off: discountPercentage,
+            name: code,
+        });
+
+        return Coupon.create({ code, discountPercentage, expirationDate, ownerId: userId, stripeCouponId: stripeCoupon.id });
+    }
+
     /**
      * Finds a coupon by the given user ID.
      * @param {string} userId - The ID of the user to find the coupon for.
      * @return {Promise<Coupon | null>} The coupon, or null if no coupon is found.
      */
-    static async getCoupon(userId) {
-        return Coupon.findOne({ ownerId: userId, isActive: true }).lean();
+    static async findOne(code, userId) {
+        return Coupon.findOne({ code, ownerId: userId, isActive: true }).lean();
     }
 
     /**
@@ -16,14 +38,14 @@ export default class CouponService {
      * @return {Promise<Object>} A validated coupon object with a subset of the coupon's data: {code, discountPercentage}.
      * @throws {Error} If the coupon is not found or has expired.
      */
-    static async validateCoupon(code) {
+    static async validateCode(code) {
         const coupon = await Coupon.findOne({ code, isActive: true });
 
         if (!coupon) {
             throw new Error("Coupon not found");
         }
 
-        if (coupon.expirationDate < new Date()) {
+        if (compareDates(coupon.expirationDate, "gt", new Date())) {
             coupon.isActive = false;
             await coupon.save();
 
