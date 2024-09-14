6f42d59b634b58ba7b0efc401c9fe855d9958ed7
Aghata Sophia <aghatasophia@gmail.com>
2024-09-13 21:37:30 -0400

    feat(coupon): Update name of the methods

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -2,7 +2,7 @@ import CouponService from "../services/coupon.service.js";
 
 export const getCoupon = async (req, res) => {
     try {
-        const coupon = await CouponService.getCoupon(req.user._id);
+        const coupon = await CouponService.findOne(req.user._id);
         res.status(200).json({ data: { coupon }, message: "Coupon found successfully" });
     } catch (error) {
         console.error("Error (getCoupon):", error);
@@ -14,7 +14,7 @@ export const validateCoupon = async (req, res) => {
     const { couponCode } = req.body;
 
     try {
-        const coupon = await CouponService.validateCoupon(couponCode);
+        const coupon = await CouponService.validateCode(couponCode);
         res.status(200).json({ data: { coupon }, message: "Coupon is valid" });
     } catch (error) {
         // TODO Add error handling
