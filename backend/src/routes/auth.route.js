db77636a2b189c29bc6eb90bb5d8b776ece27ca3
Aghata Sophia <aghatasophia@gmail.com>
2024-09-15 18:55:19 -0400

    feat(auth): Create user schemas and add them to the auth routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,14 @@
 import express from "express";
+
+import { validateSchemaMiddleware } from "../middlewares/validate-schema.middleware.js";
+import { userSignupSchema, userLoginSchema } from "../validation-schemas/user.schema.js";
+
 import { login, logout, refreshAccessToken, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/signup", validateSchemaMiddleware(userSignupSchema), signup);
+router.post("/login", validateSchemaMiddleware(userLoginSchema), login);
 router.post("/logout", logout);
 router.post("/refresh-token", refreshAccessToken);
 
