fe670450674279dcea1fbd1523add875b2d4c165
Aghata Sophia <aghatasophia@gmail.com>
2024-09-15 19:11:14 -0400

    feat(cart): Create cart schemas and add validation to the cart routes

--- backend/src/routes/cart.route.js
+++ backend/src/routes/cart.route.js
@@ -1,12 +1,15 @@
 import express from "express";
 
+import { validateSchemaMiddleware } from "../middlewares/validate-schema.middleware.js";
+import { cartItemSchema } from "../validation-schemas/cart.schemas.js";
+
 import { getCart, addItemToCart, updateItemQuantity, removeItemFromCart } from "../controllers/cart.controller.js";
 
 const router = express.Router();
 
 router.get("/", getCart);
-router.post("/item", addItemToCart);
-router.put("/item/:itemId", updateItemQuantity);
+router.post("/item", validateSchemaMiddleware(cartItemSchema), addItemToCart);
+router.put("/item/:itemId", validateSchemaMiddleware(cartItemSchema), updateItemQuantity);
 router.delete("/item/:itemId", removeItemFromCart);
 
 export default router;
