3d5417563462d2aa0103e80f23bf862d7e4e6771
Aghata Sophia <aghatasophia@gmail.com>
2024-09-20 23:24:12 -0400

    feat(order): Create method to get the total amount and numbers of completed orders in the database

--- backend/src/services/order.service.js
+++ backend/src/services/order.service.js
@@ -11,6 +11,24 @@ export default class OrderService {
     }
 
     /**
+     * Gets the total amount of all orders and the count of orders in the database.
+     * @return {Promise<{totalAmount: number, count: number}>} The total amount and count of orders.
+     */
+    static async getTotalSalesSummary() {
+        const [salesData] = Order.aggregate([
+            {
+                $group: {
+                    _id: null,
+                    total: { $sum: "$totalAmount" },
+                    count: { $sum: 1 },
+                },
+            },
+        ]);
+
+        return { totalAmount: salesData.total, count: salesData.count };
+    }
+
+    /**
      * Updates an existing order in the database.
      * @param {ObjectId} orderId - The ID of the order to update.
      * @param {Partial<Order>} order - The updated order.
