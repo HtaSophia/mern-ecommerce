a22cb7bd4205bec644e46a1e257376e53f416017
Aghata Sophia <aghatasophia@gmail.com>
2024-09-23 21:27:17 -0400

    feat(ui): Create Navbar component and add it to the App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+
+import Navbar from "./components/Navbar";
+
 function App() {
-    return <></>;
+    return (
+        <div className="min-h-screen bg-slate-100 overflow-hidden">
+            <Navbar></Navbar>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
-
