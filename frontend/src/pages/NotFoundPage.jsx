a4dd0de260f0e5bbb5af5833f41634581e1ced02
Aghata Sophia <aghatasophia@gmail.com>
2024-10-18 13:56:06 -0400

    feat(ui): Create temporary Not Found page

---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+    return (
+        <div className="flex flex-col justify-center h-svh bg-gradient-to-br from-cyan-500 to-sky-800">
+            <h1 className="text-center text-3xl font-semibold tracking-tight text-gray-900">Page not found</h1>
+        </div>
+    );
+};
+
+export default NotFoundPage;
