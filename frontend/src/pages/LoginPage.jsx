4b50425a228323a423f590135c54fa1b8a76246d
Aghata Sophia <aghatasophia@gmail.com>
2024-10-18 17:04:06 -0400

    feat(ui): Add mutation and components to the Login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import { LoaderIcon, LockIcon, LogInIcon, MailIcon } from "lucide-react";
+import { LockIcon, LogInIcon, MailIcon } from "lucide-react";
+
+import { useLoginMutation } from "../api/auth";
+import InputWithIcon from "../components/form/InputWithIcon";
+import Label from "../components/form/Label";
+import Button from "../components/Button";
 
 const LoginPage = () => {
-    const loading = false;
+    const { mutate: login, isPending } = useLoginMutation();
     const [userForm, setUserForm] = useState({ email: "", password: "" });
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(userForm);
+        login(userForm);
     };
 
     const handleChange = (event) => {
@@ -29,64 +34,39 @@ const LoginPage = () => {
 
                 <section className="mt-6">
                     <form onSubmit={handleSubmit} className="space-y-4">
-                        <div>
-                            <label htmlFor="email" className="text-sm text-gray-600">
-                                Email
-                            </label>
-                            <div className="relative mt-1 rounded-md shadow-sm">
-                                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <MailIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                </div>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    className="block w-full rounded-md border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1  ring-gray-400 placeholder:text-gray-400 focus:outline-none  focus:ring-sky-600 sm:text-sm sm:leading-6"
-                                    placeholder="andrewsmith@me.com"
-                                    value={userForm.email}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
+                        <div className="space-y-1">
+                            <Label htmlFor="email">Email</Label>
+                            <InputWithIcon
+                                name="email"
+                                id="email"
+                                type="email"
+                                placeholder="andrewsmith@me.com"
+                                value={userForm.email}
+                                onChange={handleChange}
+                                required
+                            >
+                                <MailIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                            </InputWithIcon>
                         </div>
-                        <div>
-                            <label htmlFor="password" className="text-sm text-gray-600">
-                                Password
-                            </label>
-                            <div className="relative mt-1 rounded-md shadow-sm">
-                                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <LockIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                </div>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    id="password"
-                                    className="block w-full rounded-md border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1  ring-gray-400 placeholder:text-gray-400 focus:outline-none  focus:ring-sky-600 sm:text-sm sm:leading-6"
-                                    placeholder="********"
-                                    value={userForm.password}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
+                        <div className="space-y-1">
+                            <Label htmlFor="password">Password</Label>
+                            <InputWithIcon
+                                name="password"
+                                id="password"
+                                type="password"
+                                placeholder="********"
+                                value={userForm.password}
+                                onChange={handleChange}
+                                required
+                            >
+                                <LockIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                            </InputWithIcon>
                         </div>
 
-                        <button
-                            type="submit"
-                            className="w-full py-2 px-4 flex items-center justify-center rounded-md shadow-sm font-medium text-white bg-sky-600 hover:bg-sky-500 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-600 disabled:opacity-50"
-                            disabled={loading}
-                        >
-                            {loading ? (
-                                <>
-                                    <LoaderIcon className="mr-2 h-5 w-5 animate-spin" aria-hidden="true" />
-                                    Loading...
-                                </>
-                            ) : (
-                                <>
-                                    <LogInIcon className="mr-2 h-5 w-5" aria-hidden="true" />
-                                    Login
-                                </>
-                            )}
-                        </button>
+                        <Button type="submit" isLoading={isPending}>
+                            <LogInIcon className="mr-2 h-5 w-5" aria-hidden="true" />
+                            Login
+                        </Button>
                     </form>
 
                     <p className="mt-6 text-center text-sm text-gray-600">
